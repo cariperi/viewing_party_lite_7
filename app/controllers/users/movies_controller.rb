b79a315cb28efa906428ff79cbb8d230ad0ed9fe
Caroline Peri <caroline.anne.peri@gmail.com>
2023-05-11 01:32:43 -0400

    refactor: update controller to use before_action for get_user

--- app/controllers/users/movies_controller.rb
+++ app/controllers/users/movies_controller.rb
@@ -1,15 +1,18 @@
 class Users::MoviesController < ApplicationController
+  before_action :get_user
+
   def index
-    @user = User.find(params[:user_id])
     @facade = keyword_search? ? MoviesFacade.new(params[:keyword], nil) : MoviesFacade.new
   end
 
   def show
-    @user = User.find(params[:user_id])
     @movie = MoviesFacade.new(nil, params[:id]).get_movie_details
   end
 
   private
+  def get_user
+    @user = User.find(params[:user_id])
+  end
 
   def keyword_search?
     params[:keyword].present?
