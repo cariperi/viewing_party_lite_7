46da9a711d0db3fdcb043f4b905aef2641bc4cd6
Caroline Peri <caroline.anne.peri@gmail.com>
2023-05-11 01:26:37 -0400

    feat: update movies facade with additional API calls and helper methods to format data

--- app/facades/movies_facade.rb
+++ app/facades/movies_facade.rb
@@ -1,6 +1,9 @@
 class MoviesFacade
-  def initialize(keyword = nil)
+  attr_reader :keyword, :movie_id
+
+  def initialize(keyword = nil, movie_id = nil)
     @keyword = keyword
+    @movie_id = movie_id
   end
 
   def get_movies
@@ -8,8 +11,51 @@ class MoviesFacade
 
     json = @keyword.nil? ? service.get_top_movies : service.search_movies_by_title(@keyword)
 
-    @movies = json[:results].map do |movie_data|
-      Movie.new(movie_data)
+    @movies = create_movies(json[:results])
+  end
+
+  def get_movie_details
+    service = MovieService.new
+
+    details = service.get_details(@movie_id)
+    reviews = service.get_reviews(@movie_id)
+    cast = service.get_cast(@movie_id)
+
+    @movie = create_movie(details, reviews, cast)
+  end
+
+  private
+
+  def create_movies(movie_data)
+    movie_data.map do |data|
+      Movie.new(data)
     end
+  end
+
+  def create_movie(details, reviews, cast)
+    data = {
+      id: details[:id],
+      title: details[:title],
+      vote_average: details[:vote_average],
+      runtime: details[:runtime],
+      genres: get_genres(details[:genres]),
+      summary: details[:overview],
+      url: format_url(details[:poster_path]),
+      reviews: reviews[:results],
+      cast: cast[:cast].first(10)
+    }
+    Movie.new(data)
+  end
+
+  def get_genres(data)
+    return nil if data.nil?
+
+    data.map do |genre_hash|
+      genre_hash[:name]
+    end
+  end
+
+  def format_url(data)
+    "https://image.tmdb.org/t/p/original#{data}"
   end
 end
