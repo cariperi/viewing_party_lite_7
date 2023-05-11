6474dc489776bba6e5ddaa9ea2f57e1afc9353e5
Caroline Peri <caroline.anne.peri@gmail.com>
2023-05-10 20:08:31 -0400

    feat: create MovieService and instance methods

---
+++ app/services/movie_service.rb
@@ -0,0 +1,20 @@
+class MovieService
+  def get_top_movies
+    get_url("/3/movie/top_rated")
+  end
+
+  def search_movies_by_title(keyword)
+    get_url("3/search/movie?include_adult=false&query=#{keyword}")
+  end
+
+  def get_url(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def conn
+    Faraday.new(url: "https://api.themoviedb.org") do |faraday|
+      faraday.params["api_key"] = ENV["TMDB_API_KEY"]
+    end
+  end
+end
