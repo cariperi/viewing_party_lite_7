373edad54f23c22949650804bf932e2aa5abeb3c
Caroline Peri <caroline.anne.peri@gmail.com>
2023-05-10 20:06:39 -0400

    feat: update controller with index and show actions

--- app/controllers/users/movies_controller.rb
+++ app/controllers/users/movies_controller.rb
@@ -1,5 +1,21 @@
 class Users::MoviesController < ApplicationController
   def index
     @user = User.find(params[:user_id])
+    if keyword_search?
+      @facade = MoviesFacade.new(params[:keyword])
+    else
+      @facade = MoviesFacade.new
+    end
+  end
+
+  def show
+    @user = User.find(params[:user_id])
+    #params[:id] is movie id
+  end
+
+  private
+
+  def keyword_search?
+    params[:keyword].present?
   end
 end
