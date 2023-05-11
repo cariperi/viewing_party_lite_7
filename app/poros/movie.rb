8b9997b0757f94db7db7a58f8274021753e9ef4b
Caroline Peri <caroline.anne.peri@gmail.com>
2023-05-11 01:24:34 -0400

    feat: update poro to add additional attributes

--- app/poros/movie.rb
+++ app/poros/movie.rb
@@ -1,8 +1,23 @@
 class Movie
-  attr_reader :id, :title, :vote_average
+  attr_reader :id,
+              :title,
+              :vote_average,
+              :runtime,
+              :genres,
+              :summary,
+              :image_url,
+              :reviews,
+              :cast
+
   def initialize(data)
     @id = data[:id]
     @title = data[:title]
     @vote_average = data[:vote_average]
+    @runtime = data[:runtime]
+    @genres = data[:genres]
+    @summary = data[:summary]
+    @image_url = data[:url]
+    @reviews = data[:reviews]
+    @cast = data[:cast]
   end
 end
