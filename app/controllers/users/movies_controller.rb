3c69b56137ea92f368770f9812de3c78e9084107
Caroline Peri <caroline.anne.peri@gmail.com>
2023-05-11 01:25:41 -0400

    feat: update show action in movies controller

--- app/controllers/users/movies_controller.rb
+++ app/controllers/users/movies_controller.rb
@@ -2,7 +2,7 @@ class Users::MoviesController < ApplicationController
   def index
     @user = User.find(params[:user_id])
     if keyword_search?
-      @facade = MoviesFacade.new(params[:keyword])
+      @facade = MoviesFacade.new(params[:keyword], nil)
     else
       @facade = MoviesFacade.new
     end
@@ -10,7 +10,7 @@ class Users::MoviesController < ApplicationController
 
   def show
     @user = User.find(params[:user_id])
-    #params[:id] is movie id
+    @movie = MoviesFacade.new(nil, params[:id]).get_movie_details
   end
 
   private
