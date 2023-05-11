ae2eb109201ee87e1d4555d8205ad8e9deefa07d
Caroline Peri <caroline.anne.peri@gmail.com>
2023-05-10 20:09:18 -0400

    test: update tests for user movies search results page to test for links

--- spec/features/users/movies/index_spec.rb
+++ spec/features/users/movies/index_spec.rb
@@ -18,5 +18,44 @@ RSpec.describe '/users/:id/movies', type: :feature do
 
       expect(current_path).to eq(user_discover_path(@user1))
     end
+
+    it 'I see movie titles that are links to their Movie details pages from Top Movies' do
+    VCR.use_cassette('test_for_links_top_movies', :allow_playback_repeats => true) do
+      visit user_discover_path(@user1)
+
+      click_button 'Find Top Rated Movies'
+
+      movies = MoviesFacade.new.get_movies
+      movie = movies.first
+
+      expect(page).to have_css('.title')
+
+      within(first('.movie')) do
+        click_link(movie.title)
+      end
+
+      expect(current_path).to eq(user_movie_path(@user1.id, movie.id))
+    end
+    end
+
+    it 'I see movie titles that are links to their Movie details pages from Searched Movies' do
+    VCR.use_cassette('test_for_links_search_movies', :allow_playback_repeats => true) do
+      visit user_discover_path(@user1)
+
+      fill_in :keyword, with: 'Cinderella'
+      click_button 'Search'
+
+      movies = MoviesFacade.new('Cinderella').get_movies
+      movie = movies.first
+
+      expect(page).to have_css('.title')
+
+      within(first('.movie')) do
+        click_link(movie.title)
+      end
+
+      expect(current_path).to eq(user_movie_path(@user1.id, movie.id))
+    end
+    end
   end
 end
